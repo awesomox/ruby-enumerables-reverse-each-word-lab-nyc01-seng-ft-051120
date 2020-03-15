ff1e2e3c2dec193bee533b82ade2b3d1edf696ac
tom hannaford <thom.hannaford@gmail.com>
2020-03-15 22:54:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(source)
   array_source = array.new(source)
-  array_source.each do { |string| puts string.reverse }
+  array_source.each do { |string| puts "#{string.reverse}"}
 end
